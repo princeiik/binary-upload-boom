a0075625f2447835910bdb84e34f10de3fde3aa3
Prince Kanu <kanuprince93@yahoo.com>
2023-07-31 15:30:34 -0500

    added schema for hat top bottom and shoe in model folder

--- models/Post.js
+++ models/Post.js
@@ -29,6 +29,54 @@ const PostSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  hatInfo: {
+    type: String,
+    required: true,
+  },
+  hatCost: {
+    type: Number,
+    required: true,
+  },
+  hatLocation: {
+    type: String,
+    required: true,
+  },
+  topInfo: {
+    type: String,
+    required: true,
+  },
+  topCost: {
+    type: Number,
+    required: true,
+  },
+  topLocation: {
+    type: String,
+    required: true,
+  },
+  bottomInfo: {
+    type: String,
+    required: true,
+  },
+  bottomCost: {
+    type: Number,
+    required: true,
+  },
+  bottomLocation: {
+    type: String,
+    required: true,
+  },
+  shoeInfo: {
+    type: String,
+    required: true,
+  },
+  shoeCost: {
+    type: Number,
+    required: true,
+  },
+  shoeLocation: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
